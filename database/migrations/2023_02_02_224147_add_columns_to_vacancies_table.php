deb64a15d48e0771fc7a975f93a759ab696a566d
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2023-02-03 00:44:27 +0200

    Created Second Migration

---
+++ database/migrations/2023_02_02_224147_add_columns_to_vacancies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('vacancies', function (Blueprint $table) {
+            $table->integer("sort_index")->after("expired")->nullable();
+            $table->string("city", 100)->after("sort_index")->nullable();
+            $table->string("experience", 400)->after("city")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('vacancies', function (Blueprint $table) {
+            //
+        });
+    }
+};
