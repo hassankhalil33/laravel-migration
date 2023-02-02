4a75e26c3aacf21fee28e302f80eb3e3aaf11b8a
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2023-02-03 01:11:26 +0200

    Added Auto Increment for ID in Create Vacancies Table Migration

--- database/migrations/2023_02_02_223206_create_vacancies_table.php
+++ database/migrations/2023_02_02_223206_create_vacancies_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('vacancies', function (Blueprint $table) {
-            $table->id();
+            $table->increments("id");
             $table->string("title", 200)->nullable();
             $table->string("type", 45)->nullable();
             $table->string("description", 2000)->nullable();
